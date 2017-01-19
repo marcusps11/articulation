817f4b8d37ba497b7ae3823eb244275f89f3fbc2
marcus petty-saphon <mpetty-saphon@vccp.com>
2017-01-19 16:07:14 +0000

    adding carousel

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    console.log("ready!");
+
+
+    $('.slider-display').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-display',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+});
