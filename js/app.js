f85a2a8fe28391f5b864cec32fd89fd77277ad7a
marcusps11 <marcus@1roof.com>
2017-01-21 00:07:51 +0000

    fixing carousel buttons

--- js/app.js
+++ js/app.js
@@ -16,7 +16,11 @@ $(document).ready(function() {
         dots: false,
         centerMode: true,
         focusOnSelect: true,
-        centerPadding: '200px'
+        centerPadding: '200px',
+        nextArrow: '<button class="nav-buttons">Next</button>',
+        prevArrow: '<button class="nav-buttons">Previous</button>'
+
+
 
     });
 });
